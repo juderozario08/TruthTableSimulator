a7f652955de64aff572c6bcdd744e38b605a88f3
Jude Rozario <juderzro08@gmail.com>
2024-10-12 11:19:43 -0400

    updated main

--- main.go
+++ main.go
@@ -13,14 +13,11 @@ func main() {
 	scanner.Scan()
 	expr := strings.ToLower(scanner.Text())
 	tokens, stateNames, err := GenerateTokensAndStates(expr)
-	fmt.Println(tokens)
 	if err != nil {
 		fmt.Printf("\033[91m%v\033[97m\n", err.Error())
 		return
 	}
 	terms, isPos := ParseTerms(&tokens)
-	fmt.Println(terms)
-	fmt.Println(terms)
 	states := PopulatesStateBins(tokens, stateNames)
 	states = CalculateTermBinaries(terms, isPos, states)
 	// PrintTable(stateNames, &states)
