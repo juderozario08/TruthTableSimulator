618df05bd88d55356587e33c4f3ea13450c9bd7a
Jude Rozario <juderzro08@gmail.com>
2024-10-15 08:43:20 -0400

    init eq test

--- equivalence_test.go
+++ equivalence_test.go
@@ -1,1 +1,19 @@
 package main
+
+import "testing"
+
+type TestEquivalence struct {
+	ExpectedResult bool
+	Expression1    string
+	Expression2    string
+}
+
+func TestLogicalEquivalenceCalcualtor(t *testing.T) {
+	tests := []TestEquivalence{}
+	for _, test := range tests {
+		result, _ := LogicalEquivalenceCalculator(test.Expression1, test.Expression2)
+		if test.ExpectedResult != result {
+			t.Errorf("Expected: %v\nGot: %v\n", test.ExpectedResult, result)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module simulator/main
 
 go 1.23.1
 
-require github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3 // indirect
+require github.com/golang-collections/collections v0.0.0-20130729185459-604e922904d3
